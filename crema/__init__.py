623cc6a131760a00003ee1b47f1208c0555e0b09
Brian McFee <brian.mcfee@nyu.edu>
2015-09-10 10:03:47 -0400

    checking in package scaffolding

---
+++ crema/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+'''Convolutional REpresentations for Music Analysis'''
+
+from .version import version as __version__
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+import imp
+
+version = imp.load_source('crema.version', 'crema/version.py')
+
+setup(
+    name='crema',
+    version=version.version,
+    description='Convolutional REpresentations for Music Analysis',
+    long_description='Convolutional REpresentations for Music Analysis',
+    author='CREMA developers',
+    author_email='brian.mcfee@nyu.edu',
+    url='http://github.com/bmcfee/crema',
+    download_url='http://github.com/bmcfee/crema/releases',
+    packages=find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Multimedia :: Sound/Audio :: Analysis",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+    ],
+    keywords='machine learning, audio, music',
+    license='ISC',
+    install_requires=[
+        'joblib',
+        'six',
+        'numpy',
+        'scipy',
+        'scikit-learn',
+        'pescador',
+        'librosa',
+        'lasagne',
+    ],
+    extras_require={
+        'docs': ['numpydoc']
+    }
+)
