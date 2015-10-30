cd58022ee2f05d1066d64d26fcaa19701bd48d5f
Brian McFee <brian.mcfee@nyu.edu>
2015-10-30 14:57:18 -0400

    updated version requirements

--- crema/task/instrument.py
+++ crema/task/instrument.py
@@ -8,13 +8,16 @@ from .base import BaseTaskTransformer
 
 class InstrumentTransformer(BaseTaskTransformer):
 
-    def __init__(self, jam, label_encoder):
+    def __init__(self, jam, n_samples, label_encoder):
         '''Initialize an instrument transformer
 
         Parameters
         ----------
         jam : jams.JAMS
             The JAMS object container
+
+        n_samples : int > 0
+            The number of samples in the audio frame
 
         label_encoder : sklearn.preprocessing.MultiLabelBinarizer
             The (pre-constructed) label encoder
@@ -23,4 +26,7 @@ class InstrumentTransformer(BaseTaskTransformer):
         # Grab the data frame from the first matching annotation
         data = jam.search(namespace='tag_medleydb_instruments')[0].data
 
-        # Re-index the data frame at the target sampling rate
+        # Convert the instrument activations to a matrix of activation functions
+
+    def __getitem__(self, key):
+        pass
