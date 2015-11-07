3cff543d0b70f1cb35b263af0cd8156c01dc757c
Brian McFee <brian.mcfee@nyu.edu>
2015-11-06 19:33:37 -0500

    added global label tasker

--- crema/task/tags.py
+++ crema/task/tags.py
@@ -76,3 +76,48 @@ class TimeSeriesLabelTransformer(BaseTaskTransformer):
         target = self.encode_intervals(intervals, tags)
 
         return target, mask
+
+
+class GlobalLabelTransformer(BaseTaskTransformer):
+
+    def __init__(self, namespace, labels=None):
+        '''Initialize a global label transformer
+
+        Parameters
+        ----------
+        jam : jams.JAMS
+            The JAMS object container
+        '''
+
+        super(GlobalLabelTransformer, self).__init__(namespace, 1, 1, 0)
+
+        self.encoder = MultiLabelBinarizer()
+        self.encoder.fit([labels])
+        self._classes = set(self.encoder.classes_)
+
+    def transform(self, jam):
+
+        anns = jam.search(namespace=self.namespace)
+
+        intervals = np.asarray([[0, 1]])
+        values = [None]
+        mask = False
+
+        if anns:
+            _, ann_val = anns[0].data.to_interval_values()
+            intervals = np.tile(intervals, [len(ann_val), 1])
+            values = ann_val
+            mask = True
+
+        # Suppress all intervals not in the encoder
+        tags = []
+        for v in values:
+            if v in self._classes:
+                tags.extend(self.encoder.transform([[v]]))
+            else:
+                tags.extend(self.encoder.transform([[]]))
+
+        tags = np.asarray(tags)
+        target = self.encode_intervals(intervals, tags).reshape(-1)
+
+        return target, mask
