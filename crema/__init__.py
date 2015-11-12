53f147aa25332809aa1994a07d3b76a3ee1c0b16
Brian McFee <brian.mcfee@nyu.edu>
2015-11-12 11:01:43 -0500

    introducing data generation and sampling code

--- crema/__init__.py
+++ crema/__init__.py
@@ -4,5 +4,6 @@
 from . import dsp
 from . import pre
 from . import task
+from . import data
 
 from .version import version as __version__
--- crema/pre.py
+++ crema/pre.py
@@ -9,7 +9,8 @@ from .dsp import librosa
 
 class CQT(object):
 
-    def __init__(self, n_octaves=8, over_sample=3, fmin=None, dtype=np.float32):
+    def __init__(self, n_octaves=8, over_sample=3, n_slice=2,
+                 fmin=None, dtype=np.float32):
 
         self.n_octaves = n_octaves
         self.over_sample = over_sample
@@ -19,19 +20,49 @@ class CQT(object):
 
         self.fmin = fmin
 
+        self.n_slice = n_slice
         self.dtype = dtype
 
     def extract(self, infile):
         '''Extract Constant-Q spectra from an input file'''
 
-        y, sr = librosa.load(infile)
-
-        CQT = librosa.cqt(y,
-                          n_bins=12 * self.n_octaves * self.over_sample,
-                          bins_per_octave=12 * self.over_sample,
-                          fmin=self.fmin).astype(self.dtype)
-
+        y, _ = librosa.load(infile)
         n_frames = librosa.time_to_frames(librosa.get_duration(y))
-        CQT = CQT[:n_frames]
 
-        return CQT
+        cqspec = librosa.cqt(y,
+                             n_bins=12 * self.n_octaves * self.over_sample,
+                             bins_per_octave=12 * self.over_sample,
+                             fmin=self.fmin).astype(self.dtype)
+
+        return cqspec[:, :n_frames]
+
+    def octensor(self, cqspec):
+        '''Convert a constant-q spectrogram to an octave tensor'''
+
+        # 1. pad C up to an even multiple of bins_per_octave
+
+        n_bins = cqspec.shape[0]
+        bins_per_octave = self.over_sample * 12
+        n_target = bins_per_octave * (n_bins // bins_per_octave)
+
+        if n_target > n_bins:
+            cqspec = np.pad(cqspec,
+                            [(0, n_target - n_bins), (0, 0)],
+                            mode='constant')
+
+        # 2. carve C up into n_slice * bins_per_octave tiles,
+        # strided by bins_per_octave
+        n_octaves = self.n_octaves
+
+        tensor = np.empty((cqspec.shape[1],
+                           bins_per_octave * self.n_slice,
+                           n_octaves - self.n_slice + 1),
+                          dtype=cqspec.dtype)
+
+        # Populate each sub-band
+        for i in range(n_octaves - self.n_slice + 1):
+            subband = slice(i * bins_per_octave,
+                            i * bins_per_octave + tensor.shape[1])
+            tensor[:, :, i] = cqspec[subband].T
+
+        return tensor
