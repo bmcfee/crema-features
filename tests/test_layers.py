114416382de2d0bf492d46a220d6d0b79f2425e6
Brian McFee <brian.mcfee@nyu.edu>
2015-12-16 14:16:35 -0500

    adding squeeze shape tests for conv2_layer

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -15,7 +15,7 @@ def test_conv2_layer():
 
     def __test(shape, n_filters, nl, strides, mode, squeeze):
         # Our input batch
-        x = np.random.randn(20, 16, 16, 7)
+        x = np.random.randn(20, 5, 5, 7)
 
         x_in = tf.placeholder(tf.float32, shape=x.shape, name='x')
 
@@ -29,8 +29,6 @@ def test_conv2_layer():
             sess.run(tf.initialize_all_variables())
             y = sess.run(output, feed_dict={x_in: x})
 
-        eq_(y.shape[0], x.shape[0])
-        eq_(y.shape[-1], n_filters)
 
         s1, s2 = x.shape[1:3]
 
@@ -39,18 +37,25 @@ def test_conv2_layer():
             s2 = s2 - shape[1] + 1
 
         if strides is not None:
-            s1 = s1 // strides[0]
-            s2 = s2 // strides[1]
+            s1 = s1 // strides[0] + (s1 % strides[0])
+            s2 = s2 // strides[1] + (s2 % strides[1])
 
-        eq_(y.shape[1], s1)
-        eq_(y.shape[2], s2)
+        target_shape = [x.shape[0], s1, s2, n_filters]
 
+        if squeeze is not None:
+            target_shape = [target_shape[_] for _ in range(4) if _ not in squeeze]
+
+        eq_(y.shape, tuple(target_shape))
+
+    # And a couple of squeeze tests
+    yield __test, [5, 1], 3, tf.nn.relu, None, 'VALID', [1]
+    yield __test, [1, 5], 3, tf.nn.relu, None, 'VALID', [2]
 
     for shape in [[1,3], [3, 3], [5, 1]]:
         for n_filters in [1, 2, 3]:
             for nl in [tf.nn.relu, tf.nn.relu6, tf.nn.tanh, None]:
                 for strides in [None, [min(min(shape), 2), min(min(shape), 2)]]:
                     for mode in ['SAME', 'VALID']:
-                        for squeeze in [None]:
-                            yield __test, shape, n_filters, nl, strides, mode, squeeze
+                        yield __test, shape, n_filters, nl, strides, mode, None
+
 
